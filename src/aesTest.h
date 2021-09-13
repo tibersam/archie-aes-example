9e915fa64f60b5ba885451815c4c6413ca947542
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2021-09-13 12:00:41 +0200

    add header file for aes test

---
+++ src/aesTest.h
@@ -0,0 +1,7 @@
+#ifndef AESTEST
+#define AESTEST
+
+	//Test if aes implementation behaves as expected (aka no corruption in code)
+	//Test vectors are from NIST SP 800-38A
+	int aesTest(void);
+#endif
